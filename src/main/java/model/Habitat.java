a01f4c9277f7c9ef2e65d1230c577de1aec08563
eaglor <danielsaka@hotmail.com>
2018-02-28 00:22:38 +0100

    pom updated and src getters n setters n constructors

--- src/main/java/model/Habitat.java
+++ src/main/java/model/Habitat.java
@@ -5,5 +5,38 @@ public class Habitat {
 	private Integer code;
 	private String name;
 	private String includes;
+	
+	public Habitat(Integer code, String name, String includes) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.includes = includes;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIncludes() {
+		return includes;
+	}
+
+	public void setIncludes(String includes) {
+		this.includes = includes;
+	}
+	
+	
 
 }
--- src/main/java/model/Monster.java
+++ src/main/java/model/Monster.java
@@ -5,5 +5,38 @@ public class Monster {
 	private String name;
 	private Habitat habitat;
 	private Rarity rarity;
+	
+	public Monster(String name, Habitat habitat, Rarity rarity) {
+		super();
+		this.name = name;
+		this.habitat = habitat;
+		this.rarity = rarity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Habitat getHabitat() {
+		return habitat;
+	}
+
+	public void setHabitat(Habitat habitat) {
+		this.habitat = habitat;
+	}
+
+	public Rarity getRarity() {
+		return rarity;
+	}
+
+	public void setRarity(Rarity rarity) {
+		this.rarity = rarity;
+	}
+	
+	
 
 }
