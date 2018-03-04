dd39dddc42e68ade589984245bd70cc62928e2e4
skoogiz <skoogiz@gmail.com>
2018-03-05 00:07:13 +0100

    init jpa

---
+++ src/main/java/monsterboken/core/MainApp.java
@@ -0,0 +1,25 @@
+package monsterboken.core;
+
+import javax.persistence.EntityManager;
+
+import monsterboken.db.util.JPAUtil;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+		entityManager.getTransaction().begin();
+
+		// Check database version inMemoryPU
+		String sql = "select version()";
+
+		String result = (String) entityManager.createNativeQuery(sql).getSingleResult();
+		System.out.println(result);
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+		JPAUtil.shutdown();
+
+	}
+}
---
+++ src/main/java/monsterboken/db/util/JPAUtil.java
@@ -0,0 +1,22 @@
+package monsterboken.db.util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	private static final String PERSISTENCE_UNIT_NAME = "inMemoryPU";
+    private static EntityManagerFactory factory;
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (factory == null) {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        return factory;
+    }
+
+    public static void shutdown() {
+        if (factory != null) {
+            factory.close();
+        }
+    }
+}
